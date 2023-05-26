6fbdaa33beb5bb982e38a0d631c7ce363c2666e6
Aidyn-A <31858918+Aidyn-A@users.noreply.github.com>
2023-05-26 10:22:41 -0700

    consistent namespaces

--- aten/src/ATen/native/sparse/SparseCsrTensorMath.cpp
+++ aten/src/ATen/native/sparse/SparseCsrTensorMath.cpp
@@ -127,7 +127,7 @@
 #endif
 
 #if !AT_USE_MKL_SPARSE()
-#include <ATen/native/eigen/SparseBlas.h>
+#include <ATen/native/eigen/SparseBlasImpl.h>
 #endif
 
 #include <algorithm>
@@ -671,7 +671,7 @@ Tensor& addmm_out_sparse_compressed_cpu(
 
 #if !AT_USE_MKL_SPARSE()
   if (result.layout() != kStrided && mat1.layout() != kStrided && mat2.layout() != kStrided) {
-    at::native::eigen::sparse::addmm_out_sparse(mat1, mat2, result, alpha, beta);
+    sparse::impl::eigen::addmm_out_sparse(mat1, mat2, result, alpha, beta);
     return result;
   }
   // The custom impl addmm_out_sparse_csr_native_cpu only supports CSR @
