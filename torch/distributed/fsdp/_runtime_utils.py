c99b05d271fabdfbc727285fe7e3974039c46d2b
Michael Voznesensky <voznesenskym@gmail.com>
2023-07-07 00:19:50 +0000

    wip

--- torch/distributed/fsdp/_runtime_utils.py
+++ torch/distributed/fsdp/_runtime_utils.py
@@ -1350,6 +1350,8 @@ def _register_post_backward_reshard_only_hooks(
     """
     # Construct `inp_tensors` lazily to avoid CPU overhead in typical case
     # where each flat parameter requires gradient
+    if not handle:
+        return
     inp_tensors: Optional[List[torch.Tensor]] = None
     if handle.flat_param.requires_grad:
         return
