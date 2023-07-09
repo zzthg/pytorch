4cd22814d3cbab1788c91f540980419fc8b29a8c
Michael Voznesensky <voznesenskym@gmail.com>
2023-07-09 23:23:47 +0000

    WIP

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -982,7 +982,7 @@ def get_backend(group: Optional[ProcessGroup] = None) -> str:
         pg = group
     if _rank_not_in_group(pg):
         raise RuntimeError("Invalid process group specified")
-    pg_store = _world.pg_map.get(pg, None)
+    pg_store = _world.pg_map[pg] if pg in _world.pg_map else None
     assert pg_store is not None
     return pg_store[0]
 
