21279dade145733ad54b3b96aed764e3b4c0b9c1
Shen Li <cs.shenli@gmail.com>
2023-08-14 11:34:36 -0700

    Add OnCompletion Hook to ProcessGroup
    This allows infra/trainers to get detailed stats about communication
    efficiencies without know anything about what model or distributed
    training paradigms have been used. This is helpful as infra/trainer
    package usually prefers to be as model/algorithm agnostic as possible.
    Therefore, we cannot assume that infra/trainer can have access to all
    collectives used by the model authors.
    
    This commit adds an `OnCompletion` hook to `ProcessGroupNCCL` which
    will be fired on every work completion event.
    
    ghstack-source-id: 75e97803f6e71a22e82c5ee56427b3dea10a6b92
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/106988

--- torch/csrc/distributed/c10d/ProcessGroup.hpp
+++ torch/csrc/distributed/c10d/ProcessGroup.hpp
@@ -665,6 +665,15 @@ class TORCH_API ProcessGroup : public torch::CustomClassHolder {
     return devices;
   }
 
+  void registerOnCompletionHook(
+      std::function<void(std::shared_ptr<WorkInfo>)>&& hook) {
+    getDefaultBackend()->registerOnCompletionHook(std::move(hook));
+  }
+
+  void waitForPendingWorks() {
+    getDefaultBackend()->waitForPendingWorks();
+  }
+
  protected:
   // Implementations of this interface need to call this to setup
   // appropriate logging etc.
