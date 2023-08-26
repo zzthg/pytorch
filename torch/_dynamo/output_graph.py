293ea1add67f50a9734dc0dfbaf78d8c4b1b51dc
Michael Voznesensky <voznesenskym@gmail.com>
2023-08-26 22:54:21 +0000

    Wip

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -889,7 +889,6 @@ class OutputGraph(Checkpointable[OutputGraphState]):
             )
             self.add_output_instructions(random_calls_instructions)
 
-        breakpoint()
         if (
             stack_values
             and all(
