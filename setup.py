6dc651fd248a4098666e911ce7fdc004424a383e
cyyever <cyyever@outlook.com>
2023-11-08 09:33:03 +0800

    Update setup.py
    Co-authored-by: peterbell10 <peterbell10@live.co.uk>

--- setup.py
+++ setup.py
@@ -192,8 +192,6 @@
 #   NCCL_INCLUDE_DIR
 #     specify where nccl is installed
 #
-#   NVFUSER_SOURCE_DIR
-#     specify nvfuser root directory
 #
 #   ACL_ROOT_DIR
 #     specify where Compute Library is installed
