3fa9ef12f7130e93abc53b44888f15704e483ee4
Tejas Patil <tejasp@meta.com>
2023-12-27 13:19:00 -0800

    Back out "Activation checkpoint and checkpoint_sequential errors if use_reentrant not passed explicitly (#115868)"
    Summary:
    Original commit changeset: 1c5e41f1ed20
    
    Original Phabricator Diff: D52339416
    
    Test Plan: Created using `hg backout`
    
    Reviewed By: shividhar
    
    Differential Revision: D52434604

--- test/dynamo/test_higher_order_ops.py
+++ test/dynamo/test_higher_order_ops.py
@@ -3499,9 +3499,7 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
             return torch.sigmoid(torch.matmul(x, y))
 
         def fn(x, y):
-            return torch.utils.checkpoint.checkpoint(
-                gn, torch.sin(x), y, use_reentrant=True
-            )
+            return torch.utils.checkpoint.checkpoint(gn, torch.sin(x), y)
 
         x = torch.randn(4, 4, requires_grad=True)
         y = torch.randn(4, 4, requires_grad=True)
@@ -3521,11 +3519,7 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
 
         def fn(x, y):
             return torch.utils.checkpoint.checkpoint(
-                gn,
-                torch.sin(x),
-                y,
-                use_reentrant=True,
-                preserve_rng_state=False,
+                gn, torch.sin(x), y, use_reentrant=True, preserve_rng_state=False
             )
 
         x = torch.randn(4, 4, requires_grad=True)
@@ -3545,9 +3539,7 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
             return torch.nn.functional.dropout(torch.matmul(x, y), p=0.2)
 
         def fn(x, y):
-            return torch.utils.checkpoint.checkpoint(
-                gn, torch.sin(x), y, use_reentrant=True
-            )
+            return torch.utils.checkpoint.checkpoint(gn, torch.sin(x), y)
 
         x = torch.randn(4, 4, device="cuda", requires_grad=True)
         y = torch.randn(4, 4, device="cuda", requires_grad=True)
@@ -3570,9 +3562,7 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
             return torch.nn.functional.dropout(torch.matmul(x, y), p=0.2)
 
         def fn(x, y):
-            return torch.utils.checkpoint.checkpoint(
-                gn, torch.sin(x), y, use_reentrant=True
-            )
+            return torch.utils.checkpoint.checkpoint(gn, torch.sin(x), y)
 
         x = torch.randn(4, 4, device="cuda", requires_grad=True)
         y = torch.randn(4, 4, device="cuda", requires_grad=True)
@@ -3590,11 +3580,7 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
             return torch.sigmoid(torch.matmul(x, y))
 
         def fn(x, y):
-            return torch.cos(
-                torch.utils.checkpoint.checkpoint(
-                    gn, torch.sin(x), y, use_reentrant=True
-                ),
-            )
+            return torch.cos(torch.utils.checkpoint.checkpoint(gn, torch.sin(x), y))
 
         x = torch.randn(4, 4, requires_grad=True)
         y = torch.randn(4, 4, requires_grad=True)
@@ -3627,9 +3613,7 @@ class ActivationCheckpointingTests(torch._dynamo.test_case.TestCase):
         mod = MockModule()
 
         def fn(x):
-            return torch.utils.checkpoint.checkpoint(
-                mod, torch.sin(x), use_reentrant=True
-            )
+            return torch.utils.checkpoint.checkpoint(mod, torch.sin(x))
 
         x = torch.randn(10, 10, requires_grad=True)
 
