6e62817795a4564959a21044e91ee4bf5ad1c550
Feng Yuan <feng1.yuan@intel.com>
2024-02-05 18:50:13 +0800

    Implement empty-tensor operators for XPU backend
    Features: https://github.com/pytorch/pytorch/issues/114835
    
    1. Enhance building capability for `libtorch_xpu.so` with `sycl_add_library`. `libtorch_xpu.so` is XPU backend implementation of PyTorch ATen tensor library, including
       kernel part in SYCL and host part in C++. Enable separate compilation by distinguishing SYCL and C++ sources and using SYCL and C++ compilers compile them separately.
    2. Add two Tensor factory ATen operators for XPU backend.
    
    Signed-off-by: Feng Yuan <feng1.yuanintel.com>
    
    ghstack-source-id: a0ea178ef245114a0e91206766c5d6fe6b194412
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/118073

--- test/test_xpu.py
+++ test/test_xpu.py
@@ -4,7 +4,8 @@ import sys
 import unittest
 
 import torch
-from torch.testing._internal.common_utils import NoTest, run_tests, TEST_XPU, TestCase
+from torch.testing._internal.common_utils import IS_WINDOWS, NoTest, run_tests, TEST_XPU, TestCase
+from torch.testing._internal.common_device_type import instantiate_device_type_tests
 
 if not TEST_XPU:
     print("XPU not available, skipping tests", file=sys.stderr)
@@ -46,6 +47,54 @@ class TestXpu(TestCase):
         self.assertTrue(device_capability["max_work_group_size"] > 0)
         self.assertTrue(device_capability["max_num_sub_groups"] > 0)
 
+
+# not torch.backends.xpu.is_avaliable
+@unittest.skipIf(IS_WINDOWS, "XPU operators testing is disabled")
+class TestXPUOperators(TestCase):
+    def test_empty_tensor(self):
+        # empty
+        dtypes = [torch.float, torch.bfloat16]
+        shapes = [(1, 3, 3, 3)]
+        mformats = [torch.channels_last, torch.contiguous_format]
+
+        def test_empty_tensor_(dtype, shape, mformat):
+            xpu_tensor = torch.empty(
+                shape, dtype=dtype, device=torch.device("xpu"), memory_format=mformat
+            )
+            cpu_tensor = torch.empty(shape, dtype=dtype, memory_format=mformat)
+            self.assertEqual(xpu_tensor.device(), torch.device("xpu"))
+            self.assertEqual(xpu_tensor.size(), cpu_tensor.size())
+            self.assertEqual(xpu_tensor.stride(), cpu_tensor.stride())
+            self.assertEqual(xpu_tensor.dtype(), cpu_tensor.dtype())
+
+        for dtype in dtypes:
+            for shape in shapes:
+                for mformat in mformats:
+                    test_empty_tensor_(dtype, shape, mformat)
+
+    def test_empty_strided(self):
+        # empty_strided
+        dtypes = [torch.float, torch.bfloat16]
+        shapes = [(1, 3, 3, 3)]
+        strides = [(9, 9, 3, 1), (1, 1, 1, 1)]
+
+        def test_empty_strided_(dtype, shape, stride):
+            xpu_tensor = torch.empty_strided(
+                shape, stride, dtype=dtype, device=torch.device("xpu")
+            )
+            cpu_tensor = torch.empty_strided(shape, stride, dtype=dtype)
+            self.assertEqual(xpu_tensor.device(), torch.device("xpu"))
+            self.assertEqual(xpu_tensor.size(), cpu_tensor.size())
+            self.assertEqual(xpu_tensor.stride(), cpu_tensor.stride())
+            self.assertEqual(xpu_tensor.dtype(), cpu_tensor.dtype())
+
+        for dtype in dtypes:
+            for shape in shapes:
+                for stride in strides:
+                    test_empty_strided_(dtype, shape, stride)
+
+
+instantiate_device_type_tests(TestXPUOperators, globals(), only_for=("xpu",))
 
 if __name__ == "__main__":
     run_tests()
