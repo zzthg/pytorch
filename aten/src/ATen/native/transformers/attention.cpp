7610b1b93e57887ff5ea13947a99a3c7b89c0943
Nikita Shulga <nikita.shulga@gmail.com>
2024-02-05 09:46:09 -0800

    [MPS] Add SDPA implentation
    Still work in progress, but already 2X faster than naive math one

--- aten/src/ATen/native/transformers/attention.cpp
+++ aten/src/ATen/native/transformers/attention.cpp
@@ -64,9 +64,7 @@
 #endif
 
 #include <ATen/native/nested/NestedTensorTransformerFunctions.h>
-namespace at {
-
-namespace native {
+namespace at::native {
 
 DEFINE_DISPATCH(_fused_sdp_choice_stub);
 
@@ -636,10 +634,12 @@ Tensor scaled_dot_product_attention(
     c10::optional<double> scale) {
   validate_sdpa_input(query_, key, value, attn_mask_, dropout_p, is_causal, scale);
   int64_t choice_int = static_cast<int64_t>(sdp::SDPBackend::math);
-  if (query_.device().type() == DeviceType::CUDA
-      || query_.device().type() == DeviceType::CPU
-      || query_.device().type() == DeviceType::HIP){
-    choice_int = _fused_sdp_choice_stub(query_.device().type(),
+  const auto device_type = query_.device().type();
+  if (device_type == DeviceType::CUDA
+      || device_type == DeviceType::CPU
+      || device_type == DeviceType::MPS
+      || device_type == DeviceType::HIP) {
+    choice_int = _fused_sdp_choice_stub(device_type,
       query_, key, value, attn_mask_, dropout_p, is_causal, scale);
   }
   sdp::SDPBackend backend = static_cast<sdp::SDPBackend>(choice_int);
@@ -924,5 +924,5 @@ Tensor triton_multi_head_attention(
 #endif
   return proj;
 }
-} // namespace native
-} // namespace at
+
+} // namespace at::native
