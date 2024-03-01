634647f4f58755fc01d467cd2070d66179d77efe
Simon Fan <xmfan@meta.com>
2024-02-29 19:08:20 -0800

    enable, no fullgraph

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -3194,6 +3194,7 @@ def parse_args(args=None):
         "--compiled-autograd",
         action="store_true",
         help="Enables compiled autograd on compiled benchmark",
+        default=True,
     )
 
     group_fuser = parser.add_mutually_exclusive_group()
