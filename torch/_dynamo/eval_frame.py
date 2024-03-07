e7599288d2511c447b7339929964310cd061caf5
Michael Lazos <mlazos@fb.com>
2024-03-07 18:35:08 +0000

    Don't disable init group

--- torch/_dynamo/eval_frame.py
+++ torch/_dynamo/eval_frame.py
@@ -1547,9 +1547,6 @@ class TorchPatcher:
             if opt in excluded_optimizer_classes:
                 opt.step = disable(opt.step)
 
-            if hasattr(opt, "_init_group"):
-                opt._init_group = disable(opt._init_group)
-
     @staticmethod
     def suppress_torch_distributed_warnings(fn):
         def inner_fn(*args, **kwargs):
