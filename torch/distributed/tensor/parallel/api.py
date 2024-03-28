eeea571135c061ad1e00b4ee8d1189cb4bc5b56b
Ke Wen <kw2501@meta.com>
2024-03-28 15:06:50 -0700

    [TP] Add type check for ParallelStyle
    ghstack-source-id: 9ebcd4f619f060c449f05cb03551bd299deac4be
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122920
    
    [TP] Use correct error type

--- torch/distributed/tensor/parallel/api.py
+++ torch/distributed/tensor/parallel/api.py
@@ -88,6 +88,10 @@ def parallelize_module(  # type: ignore[return]
         return parallelize_plan._apply(module, device_mesh)
     elif isinstance(parallelize_plan, dict):
         for module_path, parallelize_style in parallelize_plan.items():
+            if not isinstance(parallelize_style, ParallelStyle):
+                raise TypeError(
+                    f"Expect ParallelStyle, but got {type(parallelize_style)}!"
+                )
             sub_module = module.get_submodule(module_path)
             parent_module = module
             if "." in module_path:
@@ -103,7 +107,7 @@ def parallelize_module(  # type: ignore[return]
             )
         return module
     else:
-        raise RuntimeError(  # pyre-ignore[7]
+        raise TypeError(  # pyre-ignore[7]
             "Expect Union[ParallelStyle, Dict[str, ParallelStyle]] for"
             f" parallelize_plan, {type(parallelize_plan)} found!"
         )
