aa4189cd9fe147dc040db2d65775a3fbb98828bc
Michael Lazos <mlazos@fb.com>
2024-03-31 10:12:31 +0000

    Changes needed

--- torch/_dynamo/config.py
+++ torch/_dynamo/config.py
@@ -332,7 +332,7 @@ numpy_default_int = "int64"
 use_numpy_random_stream = False
 
 # Use C++ guard manager
-enable_cpp_guard_manager = os.environ.get("TORCHDYNAMO_CPP_GUARD_MANAGER", "0") == "1"
+enable_cpp_guard_manager = True
 
 
 def is_fbcode():
