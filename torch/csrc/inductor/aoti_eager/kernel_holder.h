4bc71e00fe48c49601b7f441c2f31e3175514e91
Wang, Eikan <eikan.wang@intel.com>
2024-04-08 06:08:30 +0000

    Add cache lookup to accelerate torch.compile for eager
    ghstack-source-id: c6d3c3c10fadd80895ca788ab4ca989ae1d7fd0f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116368

--- torch/csrc/inductor/aoti_eager/kernel_holder.h
+++ torch/csrc/inductor/aoti_eager/kernel_holder.h
@@ -3,6 +3,7 @@
 
 #include <ATen/ATen.h>
 
+#include <torch/csrc/inductor/aoti_eager/kernel_meta_info.h>
 #include <torch/csrc/inductor/aoti_runner/model_container_runner.h>
 #include <torch/csrc/utils/pybind.h>
 
@@ -20,6 +21,12 @@ class AOTIPythonKernelHolder : public c10::OperatorKernel {
   bool is_symbolic_;
   c10::optional<c10::Device> device_opt_;
 
+  std::unordered_map<
+      AOTIKernelMetaInfo,
+      std::shared_ptr<AOTIEagerKernelRunner>,
+      AOTIKernelMetaInfoHash>
+      aoti_kernel_cache_;
+
  public:
   AOTIPythonKernelHolder(
       py::object fall_back_func,
@@ -32,6 +39,15 @@ class AOTIPythonKernelHolder : public c10::OperatorKernel {
       const c10::OperatorHandle& op,
       c10::DispatchKeySet keyset,
       torch::jit::Stack* stack);
+
+ private:
+  void initAOTIKernelCache();
+  bool unpackTensors(
+      const torch::jit::Stack& stack,
+      std::vector<at::Tensor>& inputs);
+  AOTIKernelMetaInfo getInputsMetaInfo(const std::vector<at::Tensor>&);
+  std::shared_ptr<AOTIEagerKernelRunner> getAOTIEagerKernelRunner(
+      const std::string&);
 };
 
 } // namespace torch::inductor
