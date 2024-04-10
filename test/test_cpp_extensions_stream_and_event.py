e1b8e5002ac9f9d0e93f5ca5e0b274204b16ae0a
egienvalue <junluo@meta.com>
2024-04-09 22:36:04 -0700

    Add test_cpp_extensions_stream_and_event and test_cpp_extension_mtia_backend
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123614
    
    
    Add test_cpp_extensions_stream_and_event to test the generic torch.Stream/Event with fake device guard and hooks.
    Add test_cpp_extensions_mtia_backend to test the torch.mtia module's basic functionality.
    ghstack-source-id: 221870036
    @exported-using-ghexport
    ghstack-source-id: 221870036
    
    Differential Revision: [D55902506](https://our.internmc.facebook.com/intern/diff/D55902506/)

---
+++ test/test_cpp_extensions_stream_and_event.py
@@ -0,0 +1,97 @@
+# Owner(s): ["module: unknown"]
+
+import os
+import shutil
+import sys
+import tempfile
+import unittest
+
+import torch
+import torch.testing._internal.common_utils as common
+import torch.utils.cpp_extension
+from torch.testing._internal.common_utils import IS_ARM64, TEST_CUDA
+from torch.utils.cpp_extension import CUDA_HOME, ROCM_HOME
+
+
+TEST_CUDA = TEST_CUDA and CUDA_HOME is not None
+TEST_ROCM = TEST_CUDA and torch.version.hip is not None and ROCM_HOME is not None
+
+
+def remove_build_path():
+    if sys.platform == "win32":
+        # Not wiping extensions build folder because Windows
+        return
+    default_build_root = torch.utils.cpp_extension.get_default_build_root()
+    if os.path.exists(default_build_root):
+        shutil.rmtree(default_build_root, ignore_errors=True)
+
+
+@unittest.skipIf(IS_ARM64, "Does not work on arm")
+@torch.testing._internal.common_utils.markDynamoStrictTest
+class TestCppExtensionStreamAndEvent(common.TestCase):
+    """Tests Stream and Event with C++ extensions."""
+
+    module = None
+
+    def setUp(self):
+        super().setUp()
+        # cpp extensions use relative paths. Those paths are relative to
+        # this file, so we'll change the working directory temporarily
+        self.old_working_dir = os.getcwd()
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    def tearDown(self):
+        super().tearDown()
+        # return the working directory (see setUp)
+        os.chdir(self.old_working_dir)
+
+    @classmethod
+    def tearDownClass(cls):
+        remove_build_path()
+
+    @classmethod
+    def setUpClass(cls):
+        remove_build_path()
+        build_dir = tempfile.mkdtemp()
+        src = f"{os.path.abspath(os.path.dirname(__file__))}/cpp_extensions/mtia_extension.cpp"
+        # Load the fake device guard impl.
+        cls.module = torch.utils.cpp_extension.load(
+            name="mtia_extension",
+            sources=[src],
+            build_directory=build_dir,
+            extra_include_paths=[
+                "cpp_extensions",
+                "path / with spaces in it",
+                "path with quote'",
+            ],
+            extra_cflags=["-g"],
+            is_python_module=False,
+            verbose=True,
+        )
+
+    def test_stream_event(self):
+        s = torch.Stream()
+        self.assertTrue(s.device_type, int(torch._C._autograd.DeviceType.MTIA))
+        e = torch.Event()
+        self.assertTrue(e.device.type, "mtia")
+        # Should be nullptr by default
+        self.assertTrue(e.event_id == 0)
+        s.record_event(event=e)
+        print(f"recorded event 1: {e}")
+        self.assertTrue(e.event_id != 0)
+        e2 = s.record_event()
+        print(f"recorded event 2: {e2}")
+        self.assertTrue(e2.event_id != 0)
+        self.assertTrue(e2.event_id != e.event_id)
+        e.synchronize()
+        e2.synchronize()
+        time_elapsed = e.elapsed_time(e2)
+        print(f"time elapsed between e1 and e2: {time_elapsed}")
+        old_event_id = e.event_id
+        e.record(stream=s)
+        print(f"recorded event 1: {e}")
+        self.assertTrue(e.event_id == old_event_id)
+
+
+if __name__ == "__main__":
+    common.run_tests()
