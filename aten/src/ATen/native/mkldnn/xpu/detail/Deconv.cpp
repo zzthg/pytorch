9a2eeebd8c6ea472ecfe3b93c7f243fbf8188bad
ZhiweiYan-96 <zhiwei.yan@intel.com>
2024-04-11 01:29:36 +0000

    Intel GPU oneDNN Upstreaming: Convolution operators support
    ghstack-source-id: 55c1686893c0c9f10f4b6fc588fe77758aa2f94c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117529

--- aten/src/ATen/native/mkldnn/xpu/detail/Deconv.cpp
+++ aten/src/ATen/native/mkldnn/xpu/detail/Deconv.cpp
@@ -16,7 +16,7 @@ static inline dnnl::memory::dims deconv_compatible_dilation(IntArrayRef& dilatio
   return ret;
 }
 
-static inline dnnl::memory::dims deconv_dst_size(
+dnnl::memory::dims deconv_dst_size(
     IntArrayRef src_size,
     IntArrayRef weight_size,
     IntArrayRef padding,
